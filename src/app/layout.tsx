55e93a1610f8e957f4e058d0567ab31bf5383fa3
Rangga <arsyrangga@gmail.com>
2026-07-24 09:50:24 +0700

    feat: integrate Google Analytics 4 tracking script into RootLayout and refactor metadata formatting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -15,14 +16,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Rangga Arsy Prawira",
   description: "Website Portfolio Rangga Arsy Prawira",
-  openGraph : {
-    title : "Rangga Arsy Prawira",
-    description : "Website Portfolio Rangga Arsy Prawira",
-    images : "/assets/images/rangga_arsy_prawira.png"
+  openGraph: {
+    title: "Rangga Arsy Prawira",
+    description: "Website Portfolio Rangga Arsy Prawira",
+    images: "/assets/images/rangga_arsy_prawira.png",
   },
-  verification : {
-    google :"NsgML0qvjroVTwox42Mg8SxR7taic6J2XW2D3S6d0VQ"
-  }
+  verification: {
+    google: "NsgML0qvjroVTwox42Mg8SxR7taic6J2XW2D3S6d0VQ",
+  },
 };
 
 export default function RootLayout({
@@ -32,6 +33,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18345727023"
+          strategy="afterInteractive"
+        />
+        <Script id="google-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18345727023');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
