59d1d70a5ac9481737e041e268ab358d013022b9
Rangga <arsyrangga@gmail.com>
2026-08-04 13:14:20 +0700

    feat: add Siti-chan project and update project image paths

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,187 +1,186 @@
- export const projects = [
-    {
-      title: "Qlola by BRI",
-      description:
-        "QLola by BRI is BRI's latest innovative product in the form of an Integrated Corporate Solution Platform that will make it easier for Customers to access various BRI products and services",
-      gradient: "from-blue-600 to-purple-600",
-      color: "blue",
-      tech: ["Next.js", "Material UI"],
-      link: "https://qlola.bri.co.id",
-      image: "https://rangga.click/assets/images/bri.png",
-    },
-    {
-      title: "Instructor Cimb Program by Hacktive8",
-      description:
-        "Became an instructor in the Cimb program from Hacktive8, teaching frontend web development and React materials for 3 months",
-      gradient: "from-green-600 to-cyan-600",
-      color: "green",
-      tech: ["HTML", "JavaScript", "React"],
-      link: "#",
-      image: "https://rangga.click/assets/images/cimb.jpg",
-    },
-    {
-      title: "Mtoyota Mobile Apps",
-      description:
-        "mTOYOTA is an official mobile application from Toyota Astra Motor (TAM) in Indonesia, designed to make it easier for Toyota owners and prospective customers to access various services and information related to Toyota vehicles.",
-      gradient: "from-purple-600 to-pink-600",
-      color: "purple",
-      tech: ["Angular.js", "Ionic"],
-      link: "https://play.google.com/store/apps/details?id=com.toyota.m&hl=id",
-      image: "https://rangga.click/assets/images/toyota.jpg",
-    },
+export const projects = [
+  {
+    title: "Qlola by BRI",
+    description:
+      "QLola by BRI is BRI's latest innovative product in the form of an Integrated Corporate Solution Platform that will make it easier for Customers to access various BRI products and services",
+    gradient: "from-blue-600 to-purple-600",
+    color: "blue",
+    tech: ["Next.js", "Material UI"],
+    link: "https://qlola.bri.co.id",
+    image: "https://rangga.click/assets/images/bri.png",
+  },
+  {
+    title: "Qita By BRI",
+    description:
+      "Worked as a Frontend Developer on the Qita by BRI project for 2 months, focusing on the development of the SBN (Government Securities) and SIGNAL (National Digital Vehicle Tax Service) features. Responsible for building responsive user interfaces, integrating REST APIs with the backend, implementing frontend business logic, debugging and resolving issues, and collaborating closely with backend developers, QA engineers, and UI/UX designers to ensure high-quality, reliable, and user-friendly application features.",
+    gradient: "from-blue-600 to-purple-600",
+    color: "green",
+    tech: ["SWIFT", "SWIFTUI", "XCODE"],
+    link: "https://play.google.com/store/apps/details?id=id.co.bri.brimons&hl=id",
+    image: "https://portofolio.rangga.click/assets/images/qita.png",
+  },
+  {
+    title: "Mtoyota Mobile Apps",
+    description:
+      "mTOYOTA is an official mobile application from Toyota Astra Motor (TAM) in Indonesia, designed to make it easier for Toyota owners and prospective customers to access various services and information related to Toyota vehicles.",
+    gradient: "from-purple-600 to-pink-600",
+    color: "purple",
+    tech: ["Angular.js", "Ionic"],
+    link: "https://play.google.com/store/apps/details?id=com.toyota.m&hl=id",
+    image: "https://rangga.click/assets/images/toyota.jpg",
+  },
 
-    {
-      title: "Apjii Dashboard CMS",
-      description:
-        "Apjii dashboard is a CMS from Apjii, I was in charge of creating the UI for the Apjii CMS.",
-      gradient: "from-blue-600 to-purple-600",
-      color: "blue",
-      tech: ["CodeIgniter", "PHP", "Bootstrap"],
-      link: "#",
-      image: "https://rangga.click/assets/images/apjii.jpg",
-    },
-    {
-      title: "Concordia Carwash",
-      description:
-        "Concordia carwash is a website for car washing, this website aims to simplify the car washing system,  I built this website alone from start to finish.",
-      gradient: "from-blue-600 to-purple-600",
-      color: "green",
-      tech: ["CodeIgniter", "PHP", "Bootstrap"],
-      link: "#",
-      image: "https://rangga.click/assets/images/concordia.jpg",
-    },
-    {
-      title: "Rindang Pari Cahya Buana",
-      description:
-        "Rindang Pari Cahya Buana is a company profile website, this company is based in Jakarta",
-      gradient: "from-purple-600 to-cyan-600",
-      color: "purple",
-      tech: ["Next.js", "JavaScript", "Ant UI"],
-      link: "#",
-      image: "https://rangga.click/assets/images/rindang.jpg",
-    },
-    {
-      title: "Garda Medis Mobile Apps",
-      description:
-        "Garda Medis is a Doctor application, this application is based in Bali area, so patients can consult or video call and nurses can come to our home",
-      gradient: "from-purple-600 to-pink-600",
-      color: "blue",
-      tech: ["React Native", "Expo", "JavaScript"],
-      link: "#",
-      image: "https://rangga.click/assets/images/garda.jpg",
-    },
+  {
+    title: "Blogyra",
+    description:
+      "An automated SEO blog article generation system that leverages a modern architecture combining Next.js 15 App Router on the front end and Rust (Axum) on the back end, integrated with the DeepSeek AI API.",
+    gradient: "from-purple-600 to-cyan-600",
+    color: "purple",
+    tech: ["Next.js", "Axum", "Rust"],
+    link: "https://blogyra.site",
+    image: "https://portofolio.rangga.click/assets/images/blogyra.png",
+  },
+  {
+    title: "Siti Chan Mobile",
+    description:
+      "Siti Chan Mobile, an AI-powered virtual assistant application that enables users to interact with an intelligent avatar through conversations. Focused on building a responsive and intuitive mobile experience by integrating AI chat, text-to-speech capabilities, and a user-friendly interface, while optimizing performance and ensuring smooth interactions across Android devices.",
+    gradient: "from-purple-600 to-pink-600",
+    color: "blue",
+    tech: ["Kotlin", "Onnyx Runtime", "PiperTTS"],
+    link: "https://deepchan.tech",
+    image: "https://portofolio.rangga.click/assets/images/sitichan.png",
+  },
+  {
+    title: "Siti Chan Landing Page",
+    description:
+      "Developed the Siti Chan landing page to showcase the AI avatar chat platform with a modern, responsive, and user-friendly interface. Implemented interactive sections, smooth animations, and clear feature presentations to enhance user engagement while ensuring optimal performance, accessibility, and seamless responsiveness across desktop and mobile devices.",
+    gradient: "from-blue-600 to-purple-600",
+    color: "green",
+    tech: ["NextJS", "ThreeJS"],
+    link: "https://portofolio-rangga-ai.vercel.app/siti-chan",
+    image: "https://portofolio.rangga.click/assets/images/siti-web.png",
+  },
+  {
+    title: "Instructor Cimb Program by Hacktive8",
+    description:
+      "Became an instructor in the Cimb program from Hacktive8, teaching frontend web development and React materials for 3 months",
+    gradient: "from-green-600 to-cyan-600",
+    color: "green",
+    tech: ["HTML", "JavaScript", "React"],
+    link: "#",
+    image: "https://rangga.click/assets/images/cimb.jpg",
+  },
+  {
+    title: "Apjii Dashboard CMS",
+    description:
+      "Apjii dashboard is a CMS from Apjii, I was in charge of creating the UI for the Apjii CMS.",
+    gradient: "from-blue-600 to-purple-600",
+    color: "blue",
+    tech: ["CodeIgniter", "PHP", "Bootstrap"],
+    link: "#",
+    image: "https://rangga.click/assets/images/apjii.jpg",
+  },
+  {
+    title: "Rindang Pari Cahya Buana",
+    description:
+      "Rindang Pari Cahya Buana is a company profile website, this company is based in Jakarta",
+    gradient: "from-purple-600 to-cyan-600",
+    color: "purple",
+    tech: ["Next.js", "JavaScript", "Ant UI"],
+    link: "#",
+    image: "https://rangga.click/assets/images/rindang.jpg",
+  },
+  {
+    title: "Garda Medis Mobile Apps",
+    description:
+      "Garda Medis is a Doctor application, this application is based in Bali area, so patients can consult or video call and nurses can come to our home",
+    gradient: "from-purple-600 to-pink-600",
+    color: "blue",
+    tech: ["React Native", "Expo", "JavaScript"],
+    link: "#",
+    image: "https://rangga.click/assets/images/garda.jpg",
+  },
 
-    {
-      title: "Qita By BRI",
-      description:
-        "Worked as a Frontend Developer on the Qita by BRI project for 2 months, focusing on the development of the SBN (Government Securities) and SIGNAL (National Digital Vehicle Tax Service) features. Responsible for building responsive user interfaces, integrating REST APIs with the backend, implementing frontend business logic, debugging and resolving issues, and collaborating closely with backend developers, QA engineers, and UI/UX designers to ensure high-quality, reliable, and user-friendly application features.",
-      gradient: "from-blue-600 to-purple-600",
-      color: "green",
-      tech: ["SWIFT", "SWIFTUI", "XCODE"],
-      link: "https://play.google.com/store/apps/details?id=id.co.bri.brimons&hl=id",
-      image: "https://portofolio.rangga.click/assets/images/qita.jpg",
-    },
-    {
-      title: "Blogyra",
-      description:
-        "An automated SEO blog article generation system that leverages a modern architecture combining Next.js 15 App Router on the front end and Rust (Axum) on the back end, integrated with the DeepSeek AI API.",
-      gradient: "from-purple-600 to-cyan-600",
-      color: "purple",
-      tech: ["Next.js", "Axum", "Rust"],
-      link: "https://blogyra.site",
-      image: "https://portofolio.rangga.click/assets/images/blogyra.png",
-    },
-    {
-      title: "IDN Media",
-      description: "IDN Media is a company Profile site that I have worked on",
-      gradient: "from-purple-600 to-pink-600",
-      color: "blue",
-      tech: ["React Native", "Expo", "JavaScript"],
-      link: "https://idn.media",
-      image: "https://rangga.click/assets/images/idn.jpg",
-    },
+  {
+    title: "IDN Media",
+    description: "IDN Media is a company Profile site that I have worked on",
+    gradient: "from-purple-600 to-pink-600",
+    color: "blue",
+    tech: ["React Native", "Expo", "JavaScript"],
+    link: "https://idn.media",
+    image: "https://rangga.click/assets/images/idn.jpg",
+  },
 
-    {
-      title: "Bintang Jamtama Sejahtera",
-      description:
-        "Bintang Jamtama Sejahtera is a Company Profile site from my friend's office, I worked on this site alone.",
-      gradient: "from-blue-600 to-purple-600",
-      color: "green",
-      tech: ["NextJS"],
-      link: "https://bjs.vercel.app",
-      image: "https://rangga.click/assets/images/bjs.jpg",
-    },
+  {
+    title: "Bintang Jamtama Sejahtera",
+    description:
+      "Bintang Jamtama Sejahtera is a Company Profile site from my friend's office, I worked on this site alone.",
+    gradient: "from-blue-600 to-purple-600",
+    color: "green",
+    tech: ["NextJS"],
+    link: "https://bjs.vercel.app",
+    image: "https://rangga.click/assets/images/bjs.jpg",
+  },
 
-    {
-      title: "Duta Dropship",
-      description:
-        "Duta Dropship is a site created to make it easier for dropshippers to sell goods, this site was worked on by me and my team",
-      gradient: "from-purple-600 to-cyan-600",
-      color: "purple",
-      tech: ["Next.js", "NodeJS", "ANT UI"],
-      link: "https://dutadropship.vercel.app",
-      image: "https://rangga.click/assets/images/duta.jpg",
-    },
-    {
-      title: "Movementcare",
-      description:
-        "Movementcare is a donation site created for donating, I worked on this site by myself with a little help on the backend part",
-      gradient: "from-purple-600 to-pink-600",
-      color: "blue",
-      tech: ["NextJS", "NodeJS", "Xendit Payment Gateway"],
-      link: "#",
-      image: "https://rangga.click/assets/images/movementcare.jpg",
-    },
-    {
-      title: "Jejak Fun",
-      description:
-        "Jejak.fun is a website for guessing places through Google Maps API and there are scores too, the website like geoguessr, but with a different theme",
-      gradient: "from-purple-600 to-pink-600",
-      color: "green",
-      tech: ["NextJS", "NodeJS", "Google OAuth"],
-      link: "#",
-      image: "https://rangga.click/assets/images/jejak.jpg",
-    },
-    {
-      title: "Kandara Video Call",
-      description:
-        "Kandara Video Call is a website similar to Google Meet and Zoom where you can also sharescreen",
-      gradient: "from-purple-600 to-cyan-600",
-      color: "purple",
-      tech: ["Next.js", "NodeJS", "Custom CSS"],
-      link: "http://kandara-vidcall.vercel.app",
-      image: "https://rangga.click/assets/images/vc.jpg",
-    },
-
-    {
-      title: "Deepchan AI",
-      description:
-        "Deepchan AI is a site that using AI model to chat with 3D character, this project is Web3 project",
-      gradient: "from-purple-600 to-pink-600",
-      color: "blue",
-      tech: ["NextJS", "Python", "KokoroTTS"],
-      link: "https://deepchan.tech",
-      image: "https://rangga.click/assets/images/deepchan.png",
-    },
-    {
-      title: "Ekawira Sanjaya",
-      description:
-        "PT Panca Ekawira Sanjaya is a company offering distribution, construction, and application services. The company was established in Jakarta in 2018 by experienced professionals14. Their main business focuses on construction material distribution and construction services throughout Indonesia",
-      gradient: "from-purple-600 to-pink-600",
-      color: "green",
-      tech: ["NextJS", "NodeJS", "Google OAuth"],
-      link: "https://pancaekawirasanjaya.com",
-      image: "https://rangga.click/assets/images/ekawira.jpg",
-    },
-    {
-      title: "Indobara Smart Services",
-      description:
-        "Indobara Smart Services(iServe) is a website for PT Borneo Indo Bara (BIB), part of Sinar Mas Mining Group. Primarily this website is for Serving Fuel Assets, Visit Management, GA Reporting, Asset Inspection and Observation, and others. Only BIB and contractors can use Indobara Smart Services, I worked on this project in the assets management section",
-      gradient: "from-purple-600 to-cyan-600",
-      color: "purple",
-      tech: ["Nodejs", "ExpressJS", "Custom CSS"],
-      link: "https://iserve.borneo-indobara.com",
-      image: "https://rangga.click/assets/images/sinarmas_assets.png",
-    },
-  ];
+  {
+    title: "Duta Dropship",
+    description:
+      "Duta Dropship is a site created to make it easier for dropshippers to sell goods, this site was worked on by me and my team",
+    gradient: "from-purple-600 to-cyan-600",
+    color: "purple",
+    tech: ["Next.js", "NodeJS", "ANT UI"],
+    link: "https://dutadropship.vercel.app",
+    image: "https://rangga.click/assets/images/duta.jpg",
+  },
+  {
+    title: "Movementcare",
+    description:
+      "Movementcare is a donation site created for donating, I worked on this site by myself with a little help on the backend part",
+    gradient: "from-purple-600 to-pink-600",
+    color: "blue",
+    tech: ["NextJS", "NodeJS", "Xendit Payment Gateway"],
+    link: "#",
+    image: "https://rangga.click/assets/images/movementcare.jpg",
+  },
+  {
+    title: "Jejak Fun",
+    description:
+      "Jejak.fun is a website for guessing places through Google Maps API and there are scores too, the website like geoguessr, but with a different theme",
+    gradient: "from-purple-600 to-pink-600",
+    color: "green",
+    tech: ["NextJS", "NodeJS", "Google OAuth"],
+    link: "#",
+    image: "https://rangga.click/assets/images/jejak.jpg",
+  },
+  {
+    title: "Kandara Video Call",
+    description:
+      "Kandara Video Call is a website similar to Google Meet and Zoom where you can also sharescreen",
+    gradient: "from-purple-600 to-cyan-600",
+    color: "purple",
+    tech: ["Next.js", "NodeJS", "Custom CSS"],
+    link: "http://kandara-vidcall.vercel.app",
+    image: "https://rangga.click/assets/images/vc.jpg",
+  },
+  {
+    title: "Ekawira Sanjaya",
+    description:
+      "PT Panca Ekawira Sanjaya is a company offering distribution, construction, and application services. The company was established in Jakarta in 2018 by experienced professionals14. Their main business focuses on construction material distribution and construction services throughout Indonesia",
+    gradient: "from-purple-600 to-pink-600",
+    color: "green",
+    tech: ["NextJS", "NodeJS", "Google OAuth"],
+    link: "https://pancaekawirasanjaya.com",
+    image: "https://rangga.click/assets/images/ekawira.jpg",
+  },
+  {
+    title: "Indobara Smart Services",
+    description:
+      "Indobara Smart Services(iServe) is a website for PT Borneo Indo Bara (BIB), part of Sinar Mas Mining Group. Primarily this website is for Serving Fuel Assets, Visit Management, GA Reporting, Asset Inspection and Observation, and others. Only BIB and contractors can use Indobara Smart Services, I worked on this project in the assets management section",
+    gradient: "from-purple-600 to-cyan-600",
+    color: "purple",
+    tech: ["Nodejs", "ExpressJS", "Custom CSS"],
+    link: "https://iserve.borneo-indobara.com",
+    image: "https://rangga.click/assets/images/sinarmas_assets.png",
+  },
+];
 
